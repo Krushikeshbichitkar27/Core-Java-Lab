81ca3541df99ca08f629c2c28b8d10dd97c4e78a
Krushikeshbichitkar27 <111422975+Krushikeshbichitkar27@users.noreply.github.com>
2022-09-01 17:47:10 +0530

    Add files via upload

---
+++ 1 Sep/Rectangle.java
@@ -0,0 +1,45 @@
+
+/*
+Program :  Create a class named 'Rectangle' with two data members- length and breadth and a function to calculate the area which is 'length*breadth'. The class has three constructors which are :
+code by : Krushikesh
+Date :01 September 2022
+*/
+// creating class 
+class Rectangle
+{
+   private int length;
+    private int breadth;
+    Rectangle() // creating constructor having no parameter 
+    {
+        length=0;
+        breadth=0;  
+    }
+    Rectangle(int l, int b) // creating constructor with two parameter
+    {
+        length=l;
+        breadth=b;  
+    }
+    Rectangle(int n) // creating constructor with one parameter
+    {
+        length=n;
+        breadth=n;  
+    }
+	//creating method to calculate ractangle area
+    void calculate()
+    {
+        System.out.println("The area of a Rectangle is : "+ (length*breadth));
+    }
+	//calling main method
+    public static void main(String ...args)
+    {
+		//creating instance of ractangle class
+        Rectangle R1 = new Rectangle();
+        Rectangle R2 = new Rectangle(5,5);
+        Rectangle R3 = new Rectangle(6);
+		
+		//calling calculate
+        R1.calculate(); 
+        R2.calculate();
+        R3.calculate();
+    }    
+}//end code
